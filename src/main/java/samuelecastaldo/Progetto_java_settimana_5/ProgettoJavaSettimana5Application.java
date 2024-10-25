d3fa037724db5ab9d89d0a2ef4c8b5a44be5a028
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-10-25 09:55:37 +0200

    Initial commit

---
+++ src/main/java/samuelecastaldo/Progetto_java_settimana_5/ProgettoJavaSettimana5Application.java
@@ -0,0 +1,14 @@
+package samuelecastaldo.Progetto_java_settimana_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoJavaSettimana5Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProgettoJavaSettimana5Application.class, args);
+	}
+
+}
---
+++ src/test/java/samuelecastaldo/Progetto_java_settimana_5/ProgettoJavaSettimana5ApplicationTests.java
@@ -0,0 +1,13 @@
+package samuelecastaldo.Progetto_java_settimana_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoJavaSettimana5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
