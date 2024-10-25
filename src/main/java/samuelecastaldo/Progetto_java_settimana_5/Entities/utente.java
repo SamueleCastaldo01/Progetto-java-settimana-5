02e25f9583f199cdbb7bffaa95a9816d2f0c389d
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-10-25 10:12:09 +0200

    Creazione dell'entity utente

---
+++ src/main/java/samuelecastaldo/Progetto_java_settimana_5/Entities/utente.java
@@ -0,0 +1,54 @@
+package samuelecastaldo.Progetto_java_settimana_5.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "utenti")
+public class utente {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String nomeCompleto;
+    private String email;  //qui eventualmente si potrebbe fare un controllo quando faccio il save (extra)
+
+    public utente(String nomeCompleto, String email) {
+        this.nomeCompleto = nomeCompleto;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "utente{" +
+                "id=" + id +
+                ", nomeCompleto='" + nomeCompleto + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
